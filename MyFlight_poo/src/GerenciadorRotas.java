fb27d184a6d634630a204d2a75c07bdca355992f
llopes99l <62673415+llopes99l@users.noreply.github.com>
2020-03-30 15:19:02 -0300

    Update GerenciadorRotas.java

--- MyFlight_poo/src/GerenciadorRotas.java
+++ MyFlight_poo/src/GerenciadorRotas.java
@@ -1,3 +1,5 @@
+package pucrs.myflight.modelo;
+
 import java.util.ArrayList;
 
 public class GerenciadorRotas {
@@ -6,5 +8,50 @@ public class GerenciadorRotas {
     public GerenciadorRotas() {
         rotas = new ArrayList<>();
     }
+    public void adicionar(CiaAerea cia, Aeroporto origem, Aeroporto destino, GerenciadorCias.Aeronave aeronave) {
+        rotas.add(new Rota(cia, origem, destino, aeronave) );
+    }
 
+    public Rota buscarPorOrigem(Aeroporto origem) {
+        for (Rota a : rotas) {
+            if (a.getOrigem() == origem) {
+                return a;
+            }
+        }
+        return null;
+    }
+            public Rota buscarPorDestino(Aeroporto destino){
+                for (Rota h: rotas) {
+                    if (h.getDestino() == destino){
+                        return h;
+                    }
+
+                }
+        return null;
+    }
+    public Rota buscarPorCia(CiaAerea cia) {
+        for (Rota r: rotas) {
+            if (r.getCia() == cia) {
+                return r;
+            }
+        }
+        return null;
+    }
+    public Rota buscarPorAeronave(GerenciadorCias.Aeronave aeronave {
+        for (Rota y: rotas) {
+            if (y.getAeronave() == aeronave) {
+                return y;
+            }
+        }
+        return null;
+    }
+
+
+    public String listarTodas(){
+        String aux = "\nROTAS CADASTRADAS\n-----------------------------\n" ;
+        for (Rota c: rotas ) {
+            aux = aux + c.toString() + "\n";
+        }
+        return aux;
+    }
 }
