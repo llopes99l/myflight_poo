cc735a2b5d8fb4fc20b14be7744c5a3a9a37dc41
llopes99l <62673415+llopes99l@users.noreply.github.com>
2020-03-30 15:36:57 -0300

    Update GerenciadorVoos.java

--- MyFlight_poo/src/GerenciadorVoos.java
+++ MyFlight_poo/src/GerenciadorVoos.java
@@ -1,3 +1,6 @@
+package pucrs.myflight.modelo;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class GerenciadorVoos {
@@ -8,4 +11,46 @@ public class GerenciadorVoos {
         voos = new ArrayList<>();
     }
 
+    public void adicionar(Rota rota, LocalDateTime datahora, Duration duracao) {
+        voos.add(new Voo(rota, datahora, duracao));
+    }
+
+    public Voo buscarPorRota(Rota Umrota) {
+        for (Voo a : voos) {
+            if (a.getRota() == Umrota) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public Voo buscaPorDataHora(LocalDateTime UmDataHora) {
+        for (Voo a : voos) {
+            if (a.getDatahora() == UmDataHora) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public Voo buscaPorDuracao(Duration UmDuracao) {
+        for (Voo a : voos) {
+            if (a.getDuracao() == UmDuracao) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public String listarTodas(){
+        String aux = "\nROTAS CADASTRADAS\n-----------------------------\n" ;
+        for (Voo d: voos ) {
+            aux = aux + d.toString() + "\n";
+        }
+        return aux;
+    }
 }
+
+
+
+
